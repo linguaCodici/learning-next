85054794d8aad18007d60de6a5a4b40e0563c873
Victorique Pang <victoriqueee@gmail.com>
2021-09-24 12:33:54 -0500

    adding routing setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
+      <Link prefetch={ false } href={{
+        pathname: '/[username]',
+        query: { username: 'vic' },
+      }}>
+        <a>Vic's Profile</a>
+      </Link>
     </div>
   )
 }
