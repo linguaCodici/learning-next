ffed9ea31b01b18ba195943269753c2d4161b37b
Victorique Pang <victoriqueee@gmail.com>
2021-09-27 15:33:57 -0500

    adding firebase auth

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,6 +8,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -23,7 +24,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
+
 export const firestore = getFirestore(app);
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,46 @@
-export default function EnterPage({ }) {
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../lib/firebase";
+
+export default function EnterPage(props) {
+    const user = null;
+    const username = null;
+
     return (
         <main>
-            <h1>Sign Up</h1>
+            {user ? 
+                !username ? <UsernameForm /> : <SignOutButton />
+                :
+                <SignInButton />
+            }
         </main>
     )
+}
+
+function SignInButton() {
+    // const signInWithGoogle = () => { }
+    const provider = new GoogleAuthProvider();
+    
+    const signInWithGoogle = async () => {
+        await signInWithPopup(auth, provider)
+            .then(
+                (user) => console.log(user)
+            )
+            .catch(
+                (error) => console.error(error)
+            );
+    };
+
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <img src={'/google.png'} /> Sign in with Google
+        </button>
+    );
+}
+
+function SignOutButton() {
+    return <button onClick={() => auth.signOut()}>Sign Out</button>
+}
+
+function UsernameForm() {
+    return null;
 }
