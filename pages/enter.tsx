0ef5e9e1fd592d27054a04c2ee10dc2f4345426c
Victorique Pang <victoriqueee@gmail.com>
2021-09-29 09:07:16 -0500

    adding custom username form

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,49 +1,148 @@
+import { doc, getDoc, writeBatch, WriteBatch } from "@firebase/firestore";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { useContext } from "react";
+import { ChangeEvent, useCallback, useContext, useEffect, useState } from "react";
 import { UserContext } from "../lib/context";
-import { auth } from "../lib/firebase";
+import { auth, firestore } from "../lib/firebase";
+
+import { debounce } from 'debounce';
 
 export default function EnterPage(props) {
-    const {user, username} = useContext(UserContext);
-    console.log(`user exists? : ${user ? true : false}`);
-    console.log(`username exists? : ${username ? true : false}`);
+  const { user, username } = useContext(UserContext);
+  console.log(`user exists? : ${user ? true : false}`);
+  console.log(`username exists? : ${username ? true : false}`);
 
-    return (
-        <main>
-            {user ? 
-                (!username ? <UsernameForm /> : <SignOutButton />)
-                :
-                <SignInButton />
-            }
-        </main>
-    )
+  return (
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
+  );
 }
 
 function SignInButton() {
-    // const signInWithGoogle = () => { }
-    const provider = new GoogleAuthProvider();
-    
-    const signInWithGoogle = async () => {
-        await signInWithPopup(auth, provider)
-            .then(
-                (user) => console.log(user)
-            )
-            .catch(
-                (error) => console.error(error)
-            );
-    };
+  // const signInWithGoogle = () => { }
+  const provider = new GoogleAuthProvider();
 
-    return (
-        <button className="btn-google" onClick={signInWithGoogle}>
-            <img src={'/google.png'} /> Sign in with Google
-        </button>
-    );
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, provider)
+      .then((user) => console.log(user))
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} /> Sign in with Google
+    </button>
+  );
 }
 
 function SignOutButton() {
-    return <button onClick={() => auth.signOut()}>Sign Out</button>
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
 }
 
 function UsernameForm() {
-    return null;
+  const [formValue, setFormValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(UserContext);
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue])
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  const checkUsername = useCallback(debounce(async (username) => {
+    if (username.length >= 3) {
+      const ref = doc(firestore, `usernames/${username}`);
+      const exists = await getDoc(ref);
+      console.log(`Firestore read executed! ${exists.exists()}`);
+      setIsValid(!exists.exists());
+      setLoading(false);
+    }}, 
+    500),
+    [])
+  ;
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const userDoc = doc(firestore, `users/${user.uid}`);
+    const usernameDoc = doc(firestore, `usernames/${formValue}`);
+
+    const batch = writeBatch(firestore);
+
+    batch.set(userDoc, { username: formValue, photoURL: user.photoURL, displayName: user.displayName});
+    batch.set(usernameDoc, { uid: user.uid});
+
+    await batch.commit();
+  }
+
+  return (
+    !username && (
+      <section>
+        <h3>Choose Username</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            name="username"
+            placeholder="username"
+            value={formValue}
+            onChange={onChange}
+          />
+          <UsernameMessage
+            username={formValue}
+            isValid={isValid}
+            loading={loading}
+          />
+          <button type="submit" className="btn-green" disabled={!isValid}>
+            Choose
+          </button>
+
+          <h3>Debug State</h3>
+          <div>
+            Username: {formValue}
+            <br />
+            Loading: {loading.toString()}
+            <br />
+            Username Valid: {isValid.toString()}
+          </div>
+        </form>
+      </section>
+    )
+  );
+}
+
+function UsernameMessage({ username, isValid, loading}) {
+  if (loading) {
+    return <p>Checking...</p>;
+  } else if (isValid) {
+    return <p className="text-success">{username} is available!</p>;
+  } else if (username && !isValid) {
+    return <p className="text-danger">That username is taken!</p>;
+  } else {
+    return <p></p>;
+  }
 }
