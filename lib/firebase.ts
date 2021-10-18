cc9bb2546ea0492843c3e3a8de1e456d81b91f64
Victorique Pang <victoriqueee@gmail.com>
2021-10-18 10:37:37 -0500

    adding admin page and form to create new post

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -26,6 +26,7 @@ export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 export const firestore = firebase.firestore();
 
 export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+export const serverTimeStamp = firebase.firestore.FieldValue.serverTimestamp;
 
 /// helper functions
 /**
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,11 +1,89 @@
+import { useRouter } from "next/dist/client/router";
+import { useContext, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
 import AuthCheck from "../../components/AuthCheck";
+import PostFeed from "../../components/PostFeed";
+import { UserContext } from "../../lib/context";
+import { auth, firestore, serverTimeStamp } from "../../lib/firebase";
+import kebabCase from "lodash.kebabcase";
+import toast from "react-hot-toast";
 
 export default function AdminPostsPage({}) {
   return (
     <main>
       <AuthCheck>
-        <h1>Admin Posts</h1>
+        <PostList />
+        <CreateNewPost />
       </AuthCheck>
     </main>
+  );
+}
+
+function PostList() {
+  const postsRef = firestore
+    .collection("users")
+    .doc(auth.currentUser.uid)
+    .collection("posts");
+  const postsQuery = postsRef.orderBy("createdAt");
+  const [querySnapshot] = useCollection(postsQuery);
+
+  const posts = querySnapshot?.docs.map((doc) => doc.data());
+
+  return (
+    <>
+      <h1>Manage your Posts</h1>
+      <PostFeed posts={posts} admin></PostFeed>
+    </>
+  );
+}
+
+function CreateNewPost() {
+  const router = useRouter();
+  const { username } = useContext(UserContext);
+  const [title, setTitle] = useState("");
+
+  const slug = encodeURI(kebabCase(title));
+
+  const isValid = title.length > 3 && title.length < 100;
+
+  const createPost = async (e) => {
+    e.preventDefault();
+    const uid = auth.currentUser.uid;
+    const postRef = firestore.collection('users').doc(uid).collection('posts').doc(slug);
+
+    const postData = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: '# hello world!',
+      createdAt: serverTimeStamp(),
+      updatedAt: serverTimeStamp(),
+      heartCount: 0,
+    };
+
+    await postRef.set(postData);
+
+    toast.success('Post created!');
+
+    router.push(`/admin/${slug}`);
+  }
+
+  return (
+    <form onSubmit={createPost}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="My Awesome Article!"
+        // className={styles.input}
+      />
+      <p>
+        <strong>Slug: </strong> {slug}
+      </p>
+      <button type="submit" disabled={!isValid} className="btn-green">
+        Create New Post
+      </button>
+    </form>
   );
 }
